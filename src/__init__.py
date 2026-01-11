5bec0ea7830cb7a209cec3c6464d03ff3a148623
mrivassnj-svg <mrivassnj@gmail.com>
2026-01-11 15:39:16 +0000

    Add docstring and metadata to __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,13 @@
+"""
+TCJA Regime Shift Modeling Package
+----------------------------------
+Core modules for simulating corporate tax shields and 
+capital structure tradeoffs under §163(j).
+"""
 
+from .tradeoff_model import TCJAModel
+
+__all__ = ['TCJAModel']
+
+__version__ = "1.0.0"
+__author__ = "mrivassnj-svg"
